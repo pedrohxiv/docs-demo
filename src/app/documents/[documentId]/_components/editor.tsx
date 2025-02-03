3754e205483454c19fc0f54e8e4f999da5054a3b
Pedro <pedrohalmeidamendonca@gmail.com>
2025-02-03 16:41:35 -0300

    Implementing margin ruler component

--- src/app/documents/[documentId]/_components/editor.tsx
+++ src/app/documents/[documentId]/_components/editor.tsx
@@ -22,10 +22,13 @@ import { FontSize } from "@/extensions/font-size";
 import { LineHeight } from "@/extensions/line-height";
 import { useEditorStore } from "@/store/use-editor-store";
 
+import { Ruler } from "./ruler";
+
 export const Editor = () => {
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -104,6 +107,7 @@ export const Editor = () => {
 
   return (
     <div className="size-full overflow-x-auto bg-[#fafbfd] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
